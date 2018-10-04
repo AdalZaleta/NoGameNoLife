373a76964f28bc4eefc37d2f629786b99f9b2d27
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-10-04 13:03:51 -0500

    Initial commit

---
+++ Assets/_root/Character/Scripts/CharacterController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace webs
+{
+	public class CharacterController : MonoBehaviour {
+
+		public GameObject Personaje;
+		public Camera camera;
+		public float speedMove;
+
+		public Vector3[] positionsCamera;
+		int actualPosCamera = 0; 
+
+		void Awake() {
+			Manager_Static.controllerCharacter = this;
+		}
+
+		public void MoveCharacter(float _x, float _y)
+		{
+			Personaje.transform.GetComponent<Rigidbody>().velocity = new Vector3(_x * speedMove, 0, _y * speedMove);
+		}
+
+		public void ChangeCameraPosition(int _dir)
+		{
+			if(_dir > 0)
+			{
+				actualPosCamera++;
+				if(actualPosCamera > 3)
+				{
+					actualPosCamera = 0;
+				}
+				camera.transform.localPosition = positionsCamera[actualPosCamera];
+				camera.transform.LookAt(Personaje.transform);
+			}
+			else
+			{
+				actualPosCamera--;
+				if(actualPosCamera < 0)
+				{
+					actualPosCamera = 3;
+				}
+				camera.transform.localPosition = positionsCamera[actualPosCamera];
+				camera.transform.LookAt(Personaje.transform);
+			}
+		}
+	}
+}
---
+++ Assets/_root/Managers/Manager_Input.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace webs
+{
+	public class Manager_Input : MonoBehaviour {
+
+		void Awake()
+		{
+			Manager_Static.inputManager = this;
+		}
+
+		void Update()
+		{
+			//CODIGO DE LOS INPUTS DEPENDIENDO DEL ESTADO DEL JUEGO
+			if (Manager_Static.appManager.currentState == AppState.pause_menu) 
+			{
+			}
+			else if (Manager_Static.appManager.currentState == AppState.main_menu) 
+			{
+			}
+			else if (Manager_Static.appManager.currentState == AppState.gameplay) 
+			{
+
+				if(Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f) 
+				{
+					Manager_Static.controllerCharacter.MoveCharacter(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+				}
+				if(Input.GetKeyDown(KeyCode.Q))
+				{
+					Manager_Static.controllerCharacter.ChangeCameraPosition(1);
+				}
+				if(Input.GetKeyDown(KeyCode.E))
+				{
+					Manager_Static.controllerCharacter.ChangeCameraPosition(-1);
+				}
+			}
+			else if (Manager_Static.appManager.currentState == AppState.end_game) 
+			{
+			}
+			else if (Manager_Static.appManager.currentState == AppState.credits) 
+			{
+			}
+		}
+	}
+}

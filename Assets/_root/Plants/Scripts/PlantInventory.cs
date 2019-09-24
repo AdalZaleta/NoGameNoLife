ec0ee0285e78eed3eaa63d871268ed2c4f8654c0
Retus001 <retus001@gmail.com>
2019-09-24 18:53:13 -0500

    Plants SO WIP

---
+++ Assets/_root/Plants/Scripts/PlantInventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantInventory : MonoBehaviour
+{
+    private List<PlantSO> plants = new List<PlantSO>();
+    private List<PlantSO> plantCombo = new List<PlantSO>();
+
+    public void AddPlant(PlantSO _plant)
+    {
+        plants.Add(_plant);
+    }
+
+    public void ComboPlant(PlantSO _plant)
+    {
+        plantCombo.Add(_plant);
+    }
+
+    private void ExecutePlantCombo()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_root/Plants/Scripts/PlantSO.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlantSO", menuName = "Plants/MainPlant", order = 1)]
+
+public class PlantSO : ScriptableObject
+{
+    public string plantName;
+    public int rarity;
+    
+    public void EffectCure()
+    {
+
+    }
+
+    public void EffectRevive()
+    {
+
+    }
+
+    public void EffectStrength()
+    {
+
+    }
+
+    public void EffectTeleport()
+    {
+
+    }
+
+    public void EffectVelocity()
+    {
+
+    }
+
+    public void EffectLevitate()
+    {
+
+    }
+
+    public void EffectSuperjump()
+    {
+
+    }
+
+    public void EffectFire()
+    {
+
+    }
+
+    public void EffectIce()
+    {
+
+    }
+
+    public void EffectWaterbreathing()
+    {
+
+    }
+
+    public void EffectCamouflage()
+    {
+
+    }
+
+    public void EffectIlluminate()
+    {
+
+    }
+
+    public void EffectGrounded()
+    {
+
+    }
+
+    public void EffectShield()
+    {
+
+    }
+
+    public void EffectConfuse()
+    {
+
+    }
+
+    public void EffectHeal()
+    {
+
+    }
+
+}

3d5d2db2112252ad68a58a8096677815544dfee7
Ngakk <32682022+Ngakk@users.noreply.github.com>
2019-09-26 18:28:37 -0500

    Failed movement

---
+++ Assets/_root/Character/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    [Header("Settings")]
+    public float speed = 5;
+    public float angularSpeed = 5;
+
+    Rigidbody rigi;
+
+    public float vel;
+    public float axis;
+
+    private void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void Move(float _x, float _y)
+    {
+
+    }
+
+    private void Rotate(float _x, float _y)
+    {
+
+    }
+}

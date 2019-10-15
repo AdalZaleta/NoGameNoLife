8e28e0ac185a8409ee7df9927dc2817ca67f4e3d
Ngakk <32682022+Ngakk@users.noreply.github.com>
2019-10-15 17:55:35 -0500

    Intento de usar characterController fallido, avance de isGrounded

--- Assets/_root/Character/Prototype/CharacterMovement2.cs
+++ Assets/_root/Character/Prototype/CharacterMovement2.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class CharacterMovement : MonoBehaviour {
+public class CharacterMovement2 : MonoBehaviour
+{
 
     [Header("Setup")]
-    CharacterController characterController;
+    public CharacterController characterController;
     [Header("Settings")]
     public float topSpeed = 10f;
     public float acceleration = 15f;
@@ -14,26 +14,25 @@ public class CharacterMovement : MonoBehaviour {
     public float inAirSpeedModifier;
     [Tooltip("™")]
     public float jumpForce = 5;
-    public float gravityModifier = 1f;
+    public float gravity = 9.81f;
 
-    private Rigidbody rigi;
     private float currentVel = 0;
-    private bool isGrounded = true;
+    public Vector3 velocity;
 
     void Start()
     {
-        rigi = GetComponent<Rigidbody>();
+        //rigi = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
         //Testing
-        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
 
-        rigi.velocity += Physics.gravity * gravityModifier * Time.fixedDeltaTime;
+        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //rigi.velocity += Physics.gravity * gravityModifier * Time.fixedDeltaTime;
     }
 
     public void Move(float _x, float _y)
@@ -41,15 +40,26 @@ public class CharacterMovement : MonoBehaviour {
         Vector3 cameraForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up); //Vector de enfrente proyectado en un plano con normal (0,1,0)
         Vector3 cameraRight = Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up); //Vector de derecha proyectado
 
-        Vector3 verticalVelocity = Vector3.Scale(rigi.velocity, Vector3.up);
+        Vector3 verticalVelocity = Vector3.Scale(velocity, Vector3.up);
+
+        if (characterController.isGrounded)
+        {
+            verticalVelocity = Vector3.zero;
+        }
+        else
+        {
+            verticalVelocity.y -= gravity * Time.fixedDeltaTime;
+        }
+
+
         Vector3 horizontalVelocity;
 
-        float modifier = !isGrounded ? inAirSpeedModifier : 1f;
+        float modifier = !characterController.isGrounded ? inAirSpeedModifier : 1f;
 
         if (Mathf.Abs(_x) < float.Epsilon && Mathf.Abs(_y) < float.Epsilon) //Epsilon es casi igual a 0, pero comparar con esto evita problemas de redondeos de flotantes
         {
             //Si entra aquí, el jugador no se esta intentando mover
-            horizontalVelocity = rigi.velocity;//Usando los vectores de la camara + vectores de los axis para calcular la dirección final
+            horizontalVelocity = velocity;//Usando los vectores de la camara + vectores de los axis para calcular la dirección final
 
             currentVel -= slowDown * modifier * Time.fixedDeltaTime;
 
@@ -68,38 +78,23 @@ public class CharacterMovement : MonoBehaviour {
                 currentVel = topSpeed;
         }
 
-        rigi.velocity = (horizontalVelocity.normalized * currentVel) + verticalVelocity; //Aplico las velocidades
+        velocity = (horizontalVelocity.normalized * currentVel) + verticalVelocity;
+        characterController.Move(velocity * Time.fixedDeltaTime);
+        //rigi.velocity = (horizontalVelocity.normalized * currentVel) + verticalVelocity; //Aplico las velocidades
     }
 
     public void Rotate(float _x, float _y)
     {
-        rigi.angularVelocity = Vector3.zero; //Le quito la velocidad angular porque si no da vueltas solo
+        //rigi.angularVelocity = Vector3.zero; //Le quito la velocidad angular porque si no da vueltas solo
 
-        if(isGrounded)
+        if (!characterController.isGrounded)
             transform.LookAt(transform.position + new Vector3(_x, 0, _y));
     }
 
     public void Jump()
     {
-        if(isGrounded)
-            rigi.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
-        isGrounded = false;
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Floor"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    public void OnCollisionExit(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Floor"))
-        {
-            Debug.Log("Stoped touching floor");
-        }
+        if(characterController.isGrounded)
+            velocity.y += 8.0f;
+        //rigi.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
